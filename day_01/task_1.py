3186c9caa3c285a50856665593d47ba0ed184dc9
Korred <kochanskip@outlook.com>
2020-12-01 06:23:19 +0100

    Solutions for Day 1

---
+++ day_01/task_1.py
@@ -0,0 +1,21 @@
+from itertools import permutations
+from math import prod
+
+report = []
+
+# read input data
+with open("task_1_input.txt", "r") as data:
+    for line in data:
+
+        # remove entries that are too big
+        entry = int(line.strip())
+        if entry <= 2020:
+            report.append(entry)
+
+report.sort()
+
+for p in permutations(report, 2):
+    if sum(p) == 2020:
+        print(f"Entries: {p}")
+        print(f"Product: {prod(p)}")
+        break
