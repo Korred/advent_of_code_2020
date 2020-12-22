b58dd9dcaa34e623d4096b8d726eafae914673f1
Korred <kochanskip@outlook.com>
2020-12-23 00:25:35 +0100

    Solutions for Day 21

--- day_21/task_1.py
+++ day_21/task_1.py
@@ -1,9 +1,42 @@
-data = open("test_input.txt", "r").readlines()
+from re import findall
 
-food = []
+data = open("input.txt", "r").read().split("\n")
+
+food, allg_ing_mapping = list(), list()
+ingredients, matched_ingredients, matched_allergens = set(), set(), set()
+allergens_sets = dict()
+
 for line in data:
-    print(line[:-1].split(" (contains "))
-    """
-    ing, alg = map(lambda i, a: [i.split(" "), a.split(", ")], line[:-1].split("(contains"))
-    print(ing, alg)
-    """
+    ings, allgs = map(lambda x: set(findall(r"[\w']+", x)), line[:-1].split(" (contains "))
+    ingredients |= ings
+    food.append((ings, allgs))
+
+    for allergen in allgs:
+        if allergen in allergens_sets and allergen not in matched_allergens:
+            allergens_sets[allergen] &= set(ings)
+            allergens_sets[allergen] -= matched_ingredients
+
+            if len(allergens_sets[allergen]) == 1:
+                allergens_to_remove = {allergen}
+
+                while allergens_to_remove:
+                    atr = allergens_to_remove.pop()
+                    mi = allergens_sets[atr].pop()
+
+                    matched_allergens.add(atr)
+                    matched_ingredients.add(mi)
+
+                    allg_ing_mapping.append([atr, mi])
+
+                    for a in allergens_sets:
+                        allergens_sets[a] -= matched_ingredients
+                        if len(allergens_sets[a]) == 1:
+                            allergens_to_remove.add(a)
+
+        else:
+            allergens_sets[allergen] = set(ings)
+
+
+alg_free_ingredients = ingredients - matched_ingredients
+
+print(f"Occurences of allergen-free ingredients: {sum([len(alg_free_ingredients & f[0]) for f in food])}")
